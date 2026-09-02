1018a748ec2e80690d3c11dfa1cdf0da867a34d6
Fatima <fatimaarain67@gmail.com>
2026-09-02 14:55:12 +0500

    lab2

--- TABLE/sequnce_object.sql
+++ TABLE/sequnce_object.sql
@@ -5,3 +5,9 @@ CREATE SEQUENCE  OrderLinesSequence as int
 	alter SEQUENCE  OrderLinesSequence 
 	set    OrderLinesSequence  int
 	drop SEQUENCE  OrderLinesSequence 
+
+	insert into OrderDetail(OrderLineID, OrderID, OrderDate, ProductID, Quantity, UnitPrice)
+values
+    (next value for OrderLinesSequence , 1,'2025-01-15',1,2,99.99),
+	(next value for OrderLinesSequence ,  1,'2025-01-15',2,1,149.99),
+   (next  value for OrderLinesSequence,  2,'2025-01-20',1,3,220.99)
