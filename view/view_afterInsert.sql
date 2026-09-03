74aa8472bfe8cab74c3781efe86bf4360f2f0f9b
Fatima <fatimaarain67@gmail.com>
2026-09-03 17:00:39 +0500

    view

---
+++ view/view_afterInsert.sql
@@ -0,0 +1,8 @@
+insert Into Sales.Orders
+values(11,106,5,9,'2026-08-03','2026-09-03','Delivered',null,null,6,50,GETutcDATE())
+
+select * from Sales.Orders
+
+SELECT * FROM Sales.Products
+INSERT INTO Sales.Products
+VALUES(106,'SKERT','Clothing',250)
